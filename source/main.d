7bd4ab1737c8acb9e669080d6f8b4cdc6273a523
havolts <datavolts1@gmail.com>
2025-03-05 19:39:58 +0000

    Added vector.d

--- source/main.d
+++ source/main.d
@@ -1,30 +1,19 @@
-//main.d
-import window, colour;
-import std.stdio;
-import core.thread;
+//source/main.d
+import macoswindowing.window;
+import vector;
 
-extern (C)
-{
-    void pollEvents();
-}
 void main()
 {
-    Window window = new Window(1000, 1000, "My Window");
-    window.start();
-    Colour red = new Colour(255, 0, 0, 255); // Red color
+    shared Window win = new shared Window(1000, 750, "Test");
+    win.start();
+
     while (true)
     {
         pollEvents();
-        for (int i = 0; i < 1000; i++)
-        {
-            for (int j = 0; j < 1000; j++)
-            {
-                Colour random = new Colour(i, j, i * j, 255);
-                window.drawPixel(i, j, random);
-                //writeln("Pixel drawn at (", i, ", ", j, ")");
-            }
-        }
-        //writeln("Drawn");
-        Thread.sleep(1000.msecs); // Add a small delay
     }
+}
+
+struct Vertex
+{
+    Vector2 position;
 }
