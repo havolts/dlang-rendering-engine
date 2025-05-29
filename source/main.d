eaa9d1a7e566a167e395b25c37bd616e624ea9b8
havolts <datavolts1@gmail.com>
2025-05-29 16:40:40 +0300

    rewritten for new metalrendering library.

--- source/main.d
+++ source/main.d
@@ -1,19 +1,61 @@
-//source/main.d
+// renderengine/source/main.d
 import macoswindowing.window;
-import vector;
+import metalrendering;
+import std.stdio;
+import std.file;
+import core.thread;
+import std.random;
 
 void main()
 {
-    shared Window win = new shared Window(1000, 750, "Test");
+    shared Window win;
+    win = new shared Window(1000, 750, "Test");
+    win.doTerminateOnClose(true);
     win.start();
 
-    while (true)
+    MTLDevice device = MTLCreateSystemDefaultDevice();
+    CGRect frame = CGRect(0, 0, 1000, 750);
+    MTKView view = new MTKView(device, frame);
+    view.clearColor = MTLClearColor(0.0, 0.0, 1.0, 1.0);
+
+    win.setContentView(view);
+    MTLCommandQueue commandQueue = device.makeCommandQueue();
+    auto running = true;
+    while (running)
     {
         pollEvents();
-    }
-}
+        Thread.sleep(16.msecs);
 
-struct Vertex
-{
-    Vector2 position;
+        double x = uniform(0, 255);
+        double y = uniform(0, 255);
+        double z = uniform(0, 255);
+        view.clearColor = MTLClearColor(x / 255, y / 255, z / 255, 1.0);
+
+        MTLRenderPassDescriptor renderPassDescriptor = view.currentRenderPassDescriptor;
+        if (renderPassDescriptor.ptr is null)
+        {
+            writeln("rpd failed");
+            continue;
+        }
+
+        auto colorAttachment = renderPassDescriptor.colorAttachments[0];
+        colorAttachment.loadAction = MTLLoadAction.MTLLoadActionClear;
+        colorAttachment.clearColor = view.clearColor;
+
+        MTLCommandBuffer commandBuffer = commandQueue.makeCommandBuffer();
+        MTLRenderCommandEncoder renderEncoder = commandBuffer.makeRenderCommandEncoder(
+            renderPassDescriptor);
+
+        // Your rendering commands would go here
+
+        renderEncoder.endEncoding();
+
+        // Present drawable
+        MTLDrawable drawable = view.currentDrawable;
+        if (drawable.ptr !is null)
+        {
+            commandBuffer.present(drawable);
+        }
+        commandBuffer.commit();
+    }
 }
