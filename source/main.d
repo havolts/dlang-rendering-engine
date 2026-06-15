a125adb87f292c0c5b35142cb8d40a49dd5b4b16
havolts <datavolts1@gmail.com>
2026-06-15 01:26:46 +0100

    Rewrite for new metalrendering library. Added a separate thread to stop interference. Added an autorelease pool to stop memory leak crashing application.

--- source/main.d
+++ source/main.d
@@ -3,59 +3,64 @@ import macoswindowing.window;
 import metalrendering;
 import std.stdio;
 import std.file;
-import core.thread;
 import std.random;
+import std.concurrency;
+import std.math;
+import core.atomic;
+import core.time;
+import core.thread;
 
-void main()
+shared Window win;
+MTLDevice device;
+CGRect frame;
+__gshared MTKView view;
+__gshared MTLCommandQueue commandQueue;
+shared bool running = true;
+
+void renderThread()
 {
-    shared Window win;
-    win = new shared Window(1000, 750, "Test");
-    win.doTerminateOnClose(true);
-    win.start();
-
-    MTLDevice device = MTLCreateSystemDefaultDevice();
-    CGRect frame = CGRect(0, 0, 1000, 750);
-    MTKView view = new MTKView(device, frame);
-    view.clearColor = MTLClearColor(0.0, 0.0, 1.0, 1.0);
-
-    win.setContentView(view);
-    MTLCommandQueue commandQueue = device.makeCommandQueue();
-    auto running = true;
-    while (running)
+    float fpsLimit = 1000/60;
+    int fpsLimitInt = cast(int)round(fpsLimit);
+    while (atomicLoad(running))
     {
-        pollEvents();
-        Thread.sleep(16.msecs);
+        auto pool = NSAutoreleasePool.alloc().init();
+        scope(exit) pool.drain();
 
+        Thread.sleep(fpsLimitInt.msecs);
         double x = uniform(0, 255);
         double y = uniform(0, 255);
         double z = uniform(0, 255);
         view.clearColor = MTLClearColor(x / 255, y / 255, z / 255, 1.0);
-
-        MTLRenderPassDescriptor renderPassDescriptor = view.currentRenderPassDescriptor;
-        if (renderPassDescriptor.ptr is null)
-        {
-            writeln("rpd failed");
-            continue;
-        }
-
-        auto colorAttachment = renderPassDescriptor.colorAttachments[0];
-        colorAttachment.loadAction = MTLLoadAction.MTLLoadActionClear;
-        colorAttachment.clearColor = view.clearColor;
-
-        MTLCommandBuffer commandBuffer = commandQueue.makeCommandBuffer();
-        MTLRenderCommandEncoder renderEncoder = commandBuffer.makeRenderCommandEncoder(
-            renderPassDescriptor);
-
-        // Your rendering commands would go here
-
+        auto commandBuffer = commandQueue.makeCommandBuffer();
+        if (commandBuffer is null) continue;
+        auto renderPassDescriptor = view.currentRenderPassDescriptor;
+        if (renderPassDescriptor is null) continue;
+        auto renderEncoder = commandBuffer.makeRenderCommandEncoder(renderPassDescriptor);
+        if (renderEncoder is null) continue;
         renderEncoder.endEncoding();
-
-        // Present drawable
-        MTLDrawable drawable = view.currentDrawable;
-        if (drawable.ptr !is null)
-        {
-            commandBuffer.present(drawable);
-        }
+        auto drawable = view.currentDrawable;
+        if (drawable is null) continue;
+        commandBuffer.present(drawable);
         commandBuffer.commit();
+    }
+}
+void main()
+{
+    win = new shared Window(1000, 750, "Test");
+    win.doTerminateOnClose(true);
+    win.start();
+
+    frame = CGRect(CGPoint(0,0), CGSize(1000,750));
+    device = MTLCreateSystemDefaultDevice();
+    view = MTKView.alloc().initWithFrame(frame, device);
+    view.clearColor = MTLClearColor(0.0, 0.0, 1.0, 1.0);
+
+    win.setContentView(view);
+    commandQueue = device.makeCommandQueue();
+
+    spawn(&renderThread);
+    while (atomicLoad(running))
+    {
+        pollEvents();
     }
 }
