08362213d792ba099575a01532f2f3050fea2737
havolts <datavolts1@gmail.com>
2026-06-20 03:25:58 +0100

    Now render a triangle.

--- source/main.d
+++ source/main.d
@@ -15,48 +15,122 @@ MTLDevice device;
 CGRect frame;
 __gshared MTKView view;
 __gshared MTLCommandQueue commandQueue;
+
+MTLLibrary metalDefaultLibrary;
+MTLDrawable drawable;
+__gshared MTLBuffer triangleVertexBuffer;
+__gshared MTLRenderPipelineState metalRenderPSO;
 shared bool running = true;
+
+void createTriangle()
+{
+    static immutable float[9] triangleVertices =
+    [
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+    ];
+    triangleVertexBuffer = device.makeBuffer(
+        triangleVertices.ptr,
+        triangleVertices.length * float.sizeof,
+        MTLResourceOptions.storageModeShared
+    );
+}
+
+void createDefaultLibrary()
+{
+    metalDefaultLibrary = device.makeDefaultLibrary();
+    if(metalDefaultLibrary is null)
+    {
+        writeln("Failed to load default library.");
+    }
+}
+
+void createRenderPipeline()
+{
+    auto vertexShader = metalDefaultLibrary.makeFunction("vertexShader".ns);
+    auto fragmentShader = metalDefaultLibrary.makeFunction("fragmentShader".ns);
+    if(!vertexShader) writeln("vs failed");
+    if(!fragmentShader) writeln("fs failed");
+    MTLRenderPipelineDescriptor renderPipelineDescriptor = MTLRenderPipelineDescriptor.alloc().init();
+
+    renderPipelineDescriptor.label = "Triangle Rendering Pipeline".ns;
+    renderPipelineDescriptor.vertexFunction = vertexShader;
+    renderPipelineDescriptor.fragmentFunction = fragmentShader;
+    renderPipelineDescriptor.colorAttachments[0].pixelFormat = MTLPixelFormat.BGRA8Unorm;
+
+    void* error;
+    metalRenderPSO = device.makeRenderPipelineState(renderPipelineDescriptor, error);
+    if (metalRenderPSO is null) {
+        writeln("Failed to create pipeline: ", error);
+    }
+
+    renderPipelineDescriptor.release();
+}
 
 void renderThread()
 {
-    float fpsLimit = 1000/60;
-    int fpsLimitInt = cast(int)round(fpsLimit);
+    float fpsLimit = 120;
+    int fpsLimitInt = cast(int)round((1000/fpsLimit));
+    void* lastDrawablePtr = null;
+
     while (atomicLoad(running))
     {
         auto pool = NSAutoreleasePool.alloc().init();
         scope(exit) pool.drain();
-
         Thread.sleep(fpsLimitInt.msecs);
-        double x = uniform(0, 255);
-        double y = uniform(0, 255);
-        double z = uniform(0, 255);
-        view.clearColor = MTLClearColor(x / 255, y / 255, z / 255, 1.0);
+
         auto commandBuffer = commandQueue.makeCommandBuffer();
         if (commandBuffer is null) continue;
+
+        drawable = view.currentDrawable;
+        if (drawable is null) continue;
+
+        auto currentPtr = cast(void*) drawable;
+        if (currentPtr == lastDrawablePtr) continue;
+        lastDrawablePtr = currentPtr;
+
         auto renderPassDescriptor = view.currentRenderPassDescriptor;
         if (renderPassDescriptor is null) continue;
+
+        auto cd = renderPassDescriptor.colorAttachments[0];
+        cd.texture = drawable.texture;
+        cd.loadAction = MTLLoadAction.clear;
+        cd.clearColor = MTLClearColor(41.0f/255.0f, 42.0f/255.0f, 48.0f/255.0f, 1.0);
+        cd.storeAction = MTLStoreAction.store;
+
         auto renderEncoder = commandBuffer.makeRenderCommandEncoder(renderPassDescriptor);
         if (renderEncoder is null) continue;
+
+        renderEncoder.setRenderPipelineState(metalRenderPSO);
+        renderEncoder.setVertexBuffer(triangleVertexBuffer, 0, 0);
+        MTLPrimitiveType typeTriangle = MTLPrimitiveType.triangle;
+
+        size_t vertexStart = 0;
+        size_t vertexCount = 3;
+        renderEncoder.drawPrimitives(typeTriangle, vertexStart, vertexCount);
         renderEncoder.endEncoding();
-        auto drawable = view.currentDrawable;
-        if (drawable is null) continue;
         commandBuffer.present(drawable);
         commandBuffer.commit();
     }
 }
+
 void main()
 {
-    win = new shared Window(1000, 750, "Test");
-    win.doTerminateOnClose(true);
-    win.start();
-
-    frame = CGRect(CGPoint(0,0), CGSize(1000,750));
+    frame = CGRect(CGPoint(0,0), CGSize(600,600));
     device = MTLCreateSystemDefaultDevice();
     view = MTKView.alloc().initWithFrame(frame, device);
     view.clearColor = MTLClearColor(0.0, 0.0, 1.0, 1.0);
 
+    win = new shared Window(600, 600, "Test");
+    win.doTerminateOnClose(true);
     win.setContentView(view);
+    win.start();
+    win.show();
+    createTriangle();
+    createDefaultLibrary();
     commandQueue = device.makeCommandQueue();
+    createRenderPipeline();
 
     spawn(&renderThread);
     while (atomicLoad(running))
