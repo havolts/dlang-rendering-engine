3e49449ff31b4513f68a99320d72b961dd12d29d
havolts <datavolts1@gmail.com>
2026-09-26 04:02:52 +0300

    Introduced SDL3.
    main.d is mostly commented out and now just starts up a window and allows it to close. Added the framework to the .gitignore file. Updated dub.json to grab the SDL3 dependency.

--- source/main.d
+++ source/main.d
@@ -23,97 +23,133 @@ import core.time;
 import std.math;
 import std.stdio;
 
+import sdl;
+import sdl.video;
+import sdl.events;
+import sdl.error;
+
 OSXApplication app;
-OSXWindow window;
+OSXWindow osxWindow;
 CGRect frame;
 
 shared bool running = true;
 
 void main()
 {
-    app = new OSXApplication();
-    MTLDevice device = MTLCreateSystemDefaultDevice();
-
-    window = app.CreateWindow(600, 600, "Main Window");
-    window.terminateApp = true;
-
-
-    float aspectRatio = window.width/window.height;
-
-    Renderer renderer = new Renderer(device, aspectRatio);
-
-    Node[] nodes;
-
-    frame = CGRect(CGPoint(0,0), CGSize(600,600));
-    MTKView view = MTKView.alloc().initWithFrame(frame, device);
-    view.colorPixelFormat = MTLPixelFormat.BGRA8Unorm_sRGB;
-    view.depthStencilPixelFormat = MTLPixelFormat.Depth32Float;
-    view.clearColor = MTLClearColor(1.0, 0.0, 0.0, 1.0);
-    CAMetalLayer layer = view.metalLayer();
-    layer.displaySyncEnabled = false;
-
-    window.setContentView(view);
-
-    MeshLoader loader = new MeshLoader();
-
-    Mesh cube = loader.loadObj("source/assets/textured_cube.obj", renderer);
-    nodes ~= new Node(device, cube);
-    nodes ~= new Node(device, cube);
-    nodes ~= new Node(device, cube);
-    nodes ~= new Node(device, cube);
-    nodes ~= new Node(device, cube);
-    Camera camera = new Camera();
-
-    void Start()
+    if(!SDL_Init(SDL_INIT_VIDEO))
     {
-        camera.position.x = 5f;
-        camera.position.y = 5f;
-        camera.position.z = 10f;
-
-        camera.rotation.x = -1f;
-
-        foreach(i,Node node; nodes)
-        {
-            nodes[i].position.x+=i*2;
-        }
+        writeln("SDL_Init failed: ", SDL_GetError());
     }
 
-    void Update(float delta)
+    const(char) * title = "My SDL3 Window";
+    SDL_Window* window = SDL_CreateWindow(title, 800, 600, SDL_WindowFlags.SDL_WINDOW_MAXIMIZED);
+    if (window is null)
     {
-
+        writeln("SDL_CreateWindow failed: ", SDL_GetError());
+        SDL_Quit();
+        return;
     }
 
-    auto renderThread = new Thread(
-        {
-            int fpsLimit = 60;
-            float delta = 0f;
-
-            Start();
-
-            while (atomicLoad(running))
-            {
-                MonoTime frameStart = MonoTime.currTime;
-
-                Update(delta);
-                renderer.renderFrame(view, nodes, camera);
-
-                Duration elapsed = MonoTime.currTime - frameStart;
-                double targetMs = 1000.0 / fpsLimit;
-                double msToSleep = targetMs - elapsed.total!"usecs" / 1000.0;
-                if (msToSleep > 0) Thread.sleep(dur!"msecs"(cast(int) (msToSleep*4/5)));
-                while ((MonoTime.currTime - frameStart) < dur!"usecs"(cast(int)(targetMs*1000)))
-                {}
-
-                delta = (MonoTime.currTime - frameStart).total!"usecs" / 1_000_000f;
-                //writeln("FPS: ", 1f/delta);
+    SDL_Event event;
+    bool running = true;
+    while (running)
+    {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EventType.SDL_EVENT_QUIT) {
+                running = false;
             }
         }
-    );
+        SDL_Delay(10);
+    }
 
-    renderThread.start();
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
-    while (atomicLoad(running))
+/*
+
+MTLDevice device = MTLCreateSystemDefaultDevice();
+
+osxWindow = app.CreateWindow(600, 600, "Main Window");
+osxWindow.terminateApp = true;
+
+
+float aspectRatio = osxWindow.width/osxWindow.height;
+
+Renderer renderer = new Renderer(device, aspectRatio);
+
+Node[] nodes;
+
+frame = CGRect(CGPoint(0,0), CGSize(600,600));
+MTKView view = MTKView.alloc().initWithFrame(frame, device);
+view.colorPixelFormat = MTLPixelFormat.BGRA8Unorm_sRGB;
+view.depthStencilPixelFormat = MTLPixelFormat.Depth32Float;
+view.clearColor = MTLClearColor(1.0, 0.0, 0.0, 1.0);
+CAMetalLayer layer = view.metalLayer();
+layer.displaySyncEnabled = false;
+
+osxWindow.setContentView(view);
+
+MeshLoader loader = new MeshLoader();
+
+Mesh cube = loader.loadObj("source/assets/textured_cube.obj", renderer);
+nodes ~= new Node(device, cube);
+nodes ~= new Node(device, cube);
+nodes ~= new Node(device, cube);
+nodes ~= new Node(device, cube);
+nodes ~= new Node(device, cube);
+Camera camera = new Camera();
+
+void Start()
+{
+    camera.position.x = 5f;
+    camera.position.y = 5f;
+    camera.position.z = 10f;
+
+    camera.rotation.x = -1f;
+
+    foreach(i,Node node; nodes)
     {
-        pollEvents();
+        nodes[i].position.x+=i*2;
     }
 }
+
+void Update(float delta)
+{
+
+}
+
+auto renderThread = new Thread(
+    {
+        int fpsLimit = 60;
+        float delta = 0f;
+
+        Start();
+
+        while (atomicLoad(running))
+        {
+            MonoTime frameStart = MonoTime.currTime;
+
+            Update(delta);
+            renderer.renderFrame(view, nodes, camera);
+
+            Duration elapsed = MonoTime.currTime - frameStart;
+            double targetMs = 1000.0 / fpsLimit;
+            double msToSleep = targetMs - elapsed.total!"usecs" / 1000.0;
+            if (msToSleep > 0) Thread.sleep(dur!"msecs"(cast(int) (msToSleep*4/5)));
+            while ((MonoTime.currTime - frameStart) < dur!"usecs"(cast(int)(targetMs*1000)))
+            {}
+
+            delta = (MonoTime.currTime - frameStart).total!"usecs" / 1_000_000f;
+            //writeln("FPS: ", 1f/delta);
+        }
+    }
+);
+
+renderThread.start();
+
+while (atomicLoad(running))
+{
+    pollEvents();
+}
+*/
